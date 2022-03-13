f47412b5508513b8e719fd123984d3a927d47b7b
honeykikiki <tjdwls75666@naver.com>
2022-03-13 19:35:08 +0900

    first commit

---
+++ puthonWorkSpace/practice.py
@@ -0,0 +1,34 @@
+# print('풍선')
+# print("나비")
+# print("나비" * 3)
+
+# print(5 > 10)
+# print(5 < 10)
+# print(True)
+# print(False)
+# print(not True)
+# print(not (5 > 10))
+
+#애완동물은 소개해 주세요
+animal = "강아지"
+name = '연탄이'
+age = 4
+hobby = '산책'
+is_adult = age >= 3
+
+'''
+이렇게 
+하면 
+여러 
+문장이 
+주석
+'''
+
+# print("우립집 " + animal + "의 "+ name + "은 연탄")
+# hobby = "곰돌이"
+# # print( name + "는 "+ str(age) +"살이며, "+ hobby +"을 아주 좋아해요")
+# print( name + "는 ", age, "살이며, ", hobby, "을 아주 좋아해요")
+# print(name + "는 어른일까요?" + str(is_adult))
+
+station = "사당 "
+print(station+'행 열차가 들어오고 있습니다.')
