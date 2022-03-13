22b8243e02213bf0df412765bf18de50bbdc1302
honeykikiki <tjdwls75666@naver.com>
2022-03-13 20:32:58 +0900

    파이썬 기본 문법

--- puthonWorkSpace/practice.py
+++ puthonWorkSpace/practice.py
@@ -30,5 +30,60 @@ is_adult = age >= 3
 # print( name + "는 ", age, "살이며, ", hobby, "을 아주 좋아해요")
 # print(name + "는 어른일까요?" + str(is_adult))
 
-station = "사당 "
-print(station+'행 열차가 들어오고 있습니다.')
+# station = "사당"
+# print(station+'행 열차가 들어오고 있습니다.')
+# station = "신도림"
+# print(station+'행 열차가 들어오고 있습니다.')
+# station = "인청공항"
+# print(station+'행 열차가 들어오고 있습니다.')
+
+# print(1+1) # 2
+# print(1-3) # -2
+# print(5*3) # 15
+# print(6/4) # 1.5
+
+# print(2**3) # 8
+# print(5%3) # 2
+# print(10%3) # 1
+# print(5//3) # 1 몫
+# print(10//3) # 3
+
+# print(10 > 3) # True
+# print(4 >= 7) # False
+
+# print(3 == 3) # True
+# print(4 == 2) # false
+# print(4 + 3 == 7) # True
+
+# print(1 != 3)
+# print(not(1 != 3))
+
+# print((3 > 0) and (3 < 5)) # True
+# print((3 > 0) & (3 < 5)) # True
+# print((3 > 0) & (3 > 5)) # False
+
+# print((3 > 0) or (3 > 5)) # True
+# print((3 > 0) | (3 > 5)) # True
+
+# print(5 > 4 > 3) # True
+# print(5 > 4 > 7) # False
+
+print(2 + 3 * 4) # 14
+print((2 + 3) * 4) # 20
+
+number  = 2 + 3 * 4 
+print(number) # 14
+number = number + 2
+print(number) # 16
+number +=  2
+print(number) # 18
+number *=  2
+print(number) # 36
+number /=  2
+print(number) # 18
+number -=  2
+print(number) # 16
+
+number %=  5
+print(number) # 1
+
